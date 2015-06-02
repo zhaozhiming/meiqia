7bca6c3f5f06734c8d3d516fb5c61623c8e81d74
zhaozhiming <kingzzm1982@sina.com>
2015-06-02 08:25:52 +0800

    finish

---
+++ Question6.py
@@ -0,0 +1,24 @@
+import socket
+import sys
+
+
+def socket_server(host='localhost', port=900):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.bind((host, port))
+    except socket.error as msg:
+        print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+        sys.exit()
+
+    s.listen(10)
+
+    while 1:
+        conn, addr = s.accept()
+        buf = conn.recv(64)
+        if len(buf) > 0:
+            print buf
+
+    s.close()
+
+if __name__ == '__main__':
+    socket_server()
